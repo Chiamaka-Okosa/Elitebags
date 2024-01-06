be909890cdabdeb61dff0a813561dd2f7f9ee0be
Onadebi <onadebi@onadebi.com>
2024-01-06 15:13:41 -0500

    Refactoring of pages and 'service' page error fix

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Service = () => {
   return (
     <section id="sectionBody" style={{display:'flex',gap:'10px', justifyContent:'space-between', padding:'15px'}}>
-    <div ClassName="App"> 
+    <div className="App"> 
       <h2 style={{color:'blue',textDecoration:'underline'}}>Our Service</h2>
       <br/><br/>
      <img src="https://media.istockphoto.com/id/1365118618/photo/blue-fashion-purse-handbag-on-white-background-isolated.webp?b=1&s=170667a&w=0&k=20&c=R6fr0o1pCkSFP8Q6q84M8NraLXXlMHtNG93fx8G_NYY=" alt="Hand bag" width="500px" height="400px" />
@@ -15,7 +15,7 @@ const Service = () => {
       <form>
         
         
-    <label for="condition">Condition</label> 
+    <label htmlFor="condition">Condition</label> 
      <select id="condition">
        <option>Brand new</option>
        <option>Refurbished</option>
@@ -23,20 +23,20 @@ const Service = () => {
      </select>
     <p/>
     <h4 style={{backgroundColor:'orangered', padding:'10px' }}>Bag variants</h4>
-    <input type='checkbox' id='skyblueleathershoulderbag' value='skyblueleathershoulderbag'>Sky Blue Leather Shoulder Bag</input>
+    <input type='checkbox' id='skyblueleathershoulderbag' value='skyblueleathershoulderbag'/> Sky Blue Leather Shoulder Bag
     <p/>
-    <input type='checkbox' id='siliconeyellowmediumshoulderbag' value='siliconeyellowmediumshoulderbag'>Silicone Yellow Medium Shoulder Bag</input>
+    <input type='checkbox' id='siliconeyellowmediumshoulderbag' value='siliconeyellowmediumshoulderbag'/> Silicone Yellow Medium Shoulder Bag
     <p/>
 
     <section>
       <h5 style={{backgroundColor:'green', fontSize: '24px', color:'blanchedalmond',padding:'10px',marginBottom: '15px' }}
       >Delivery option</h5>
       <input type='radio' id='pickup' name='delivery' value='pickup'/>
-      <label for='pickup'>Pick up</label>
+      <label htmlFor='pickup'>Pick up</label>
       <input type='radio' id='house_delivery' name='delivery'/>
-      <label for='house_delivery'>Home delivery</label>
+      <label htmlFor='house_delivery'>Home delivery</label>
       <input type='radio' id='express_house_delivery' name='delivery'/>
-      <label for='express_house_delivery'>Express Home delivery</label>
+      <label htmlFor='express_house_delivery'>Express Home delivery</label>
     </section>
 
     <p>
@@ -44,7 +44,7 @@ const Service = () => {
       <br/>
       <textarea name="message" id="" cols="30" rows="10"></textarea>
     </p>
-    <input button type='reset' value='Reset'/>  <button type='submit'>Submit</button>
+    <button type='reset' value='Reset'/>  <button type='submit'>Submit</button>
 
 
         </form>
